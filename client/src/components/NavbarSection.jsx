808fe20379cde71745ea42bea999ed1a8d218268
Máté Szakasits <120025444+mate153@users.noreply.github.com>
2024-01-17 19:56:37 +0100

    [ UPDATE ] Basic navbar added

---
+++ client/src/components/NavbarSection.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function NavbarSection() {
+  return (
+    <Navbar expand="lg" bg="dark" data-bs-theme="dark" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Mate Szakasits</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav  className="me-auto justify-content-end" style={{width: '100%'}}>
+              <Nav.Link href="#welcome-section">Home</Nav.Link>
+              <Nav.Link href="#about-me-section">Features</Nav.Link>
+              <Nav.Link href="#projects-section">Projects</Nav.Link>
+              <Nav.Link href="#contact-section">Contact</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default NavbarSection

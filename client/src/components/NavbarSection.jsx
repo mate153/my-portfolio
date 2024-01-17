f50b389d3ed0261d6115772446c4504d76244591
Máté Szakasits <120025444+mate153@users.noreply.github.com>
2024-01-17 20:32:04 +0100

    [ UPDATE ] About me typo fixed

--- client/src/components/NavbarSection.jsx
+++ client/src/components/NavbarSection.jsx
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import './style/NavbarSection.css'
 
 function NavbarSection() {
   return (
@@ -13,7 +14,7 @@ function NavbarSection() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav  className="me-auto justify-content-end" style={{width: '100%'}}>
               <Nav.Link href="#welcome-section">Home</Nav.Link>
-              <Nav.Link href="#about-me-section">Features</Nav.Link>
+              <Nav.Link href="#about-me-section">About Me</Nav.Link>
               <Nav.Link href="#projects-section">Projects</Nav.Link>
               <Nav.Link href="#contact-section">Contact</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">

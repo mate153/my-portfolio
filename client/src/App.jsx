ce23b580c8d72b58723122cfa708e4f4014a118d
Máté Szakasits <120025444+mate153@users.noreply.github.com>
2024-01-17 19:54:05 +0100

    [ UPDATE ] Components / sections added to navigate

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Container } from 'react-bootstrap';
+import AboutMeSection from './components/AboutMeSection.jsx'
+import ContactSection from './components/ContactSection.jsx'
+import NavbarSection from './components/NavbarSection.jsx'
+import ProjectsSection from './components/ProjectsSection.jsx'
+import WelcomeSection from './components/WelcomeSection.jsx'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Container fluid style={{ padding: 0, margin: 0, width: '100%' }}>
+      {/*NAVBAR SECTION*/}
+      <section id="navbar-section">
+        <NavbarSection />
+      </section>
+
+      {/*WELCOME SECTION*/}
+      <section id="welcome-section">
+        <WelcomeSection />
+      </section>
+
+      {/*ABOUT ME SECTION*/}
+      <section id="about-me-section">
+        <AboutMeSection />
+      </section>
+
+      {/*PROJECTS SECTION*/}
+      <section id="projects-section">
+        <ProjectsSection />
+      </section>
+
+      {/*CONTACT SECTION*/}
+      <section id="contact-section">
+        <ContactSection />
+      </section>
+    </Container>
   )
 }
 

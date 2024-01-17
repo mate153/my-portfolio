84b70fb30b81eff5d43219212ea440d6fd335158
Máté Szakasits <120025444+mate153@users.noreply.github.com>
2024-01-17 23:16:23 +0100

    [ UPDATE ] Unnecessary dropdown items deleted

--- client/src/components/NavbarSection.jsx
+++ client/src/components/NavbarSection.jsx
@@ -13,20 +13,13 @@ function NavbarSection() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav  className="me-auto justify-content-end" style={{width: '100%'}}>
-              <Nav.Link href="#welcome-section">Home</Nav.Link>
-              <Nav.Link href="#about-me-section">About Me</Nav.Link>
-              <Nav.Link href="#projects-section">Projects</Nav.Link>
-              <Nav.Link href="#contact-section">Contact</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
+            <Nav.Link href="#welcome-section">Home</Nav.Link>
+            <Nav.Link href="#about-me-section">About Me</Nav.Link>
+            <Nav.Link href="#projects-section">Projects</Nav.Link>
+            <Nav.Link href="#contact-section">Contact</Nav.Link>
+            <NavDropdown title="Language" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">English</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Hungarian</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>

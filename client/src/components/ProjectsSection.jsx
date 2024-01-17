885d3ea4e2c65ebd7a0da737bb7782d85fcb7f4e
Máté Szakasits <120025444+mate153@users.noreply.github.com>
2024-01-17 22:04:30 +0100

    [ UPDATE ] Basic project section added

--- client/src/components/ProjectsSection.jsx
+++ client/src/components/ProjectsSection.jsx
@@ -1,9 +1,71 @@
 import React from 'react'
+import { Row, Col, Card, Button } from 'react-bootstrap';
 import './style/ProjectsSection.css'
 
 function ProjectsSection() {
   return (
-    <div>ProjectsSection</div>
+    <Row className='border text-center align-items-center test project-section-container' style={{height: '80vh', width: '100%'}}>
+      <Col className='d-flex'>
+          <div className='align-items-center mx-auto'>
+          <Card className='card'>
+              <Card.Img variant="top" src="./vite.svg" className='card-img' />
+              <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                  Some quick example text to build on the card title and make up the
+                  bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+          </Card>
+          </div>
+      </Col>
+      <Col className='d-flex'>
+          <div className='align-items-center mx-auto'>
+          <Card className='card card-skills'>
+              <Card.Img variant="top" src="./vite.svg" className='card-img' />
+              <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                  Some quick example text to build on the card title and make up the
+                  bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+          </Card>
+          </div>
+      </Col>
+      <Col className='d-flex'>
+          <div className='align-items-center mx-auto'>
+          <Card className='card'>
+              <Card.Img variant="top" src="./vite.svg" className='card-img' />
+              <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                  Some quick example text to build on the card title and make up the
+                  bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+          </Card>
+          </div>
+      </Col>
+      <Col className='d-flex'>
+          <div className='align-items-center mx-auto'>
+          <Card className='card'>
+              <Card.Img variant="top" src="./vite.svg" className='card-img' />
+              <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                  Some quick example text to build on the card title and make up the
+                  bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+          </Card>
+          </div>
+      </Col>
+  </Row>
   )
 }
 

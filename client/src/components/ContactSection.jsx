e04c2261f88c22aa9394bcf15a4e16721b74ec19
Máté Szakasits <120025444+mate153@users.noreply.github.com>
2024-01-17 23:14:24 +0100

    [ UPDATE ] Get in touch form added

--- client/src/components/ContactSection.jsx
+++ client/src/components/ContactSection.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import { Container, Form, FormGroup, FormLabel, FormControl, Button, Row, Col } from 'react-bootstrap';
 import './style/ContactSection.css'
 
 function ContactSection() {
-  return (
-    <div>ContactSection</div>
+  return (    
+    <Row>
+      <Col className='text-center p-5' lg={8}>
+        <span className='form-title'>Get in touch</span>
+        <Container className="form-container text-start p-4" bg="dark" data-bs-theme="dark">
+          <Form className="form">
+            <FormGroup className="form-group">
+              <FormLabel htmlFor="text" className="form-label mx-1">Name</FormLabel>
+              <FormControl required name="name" id="name" type="text" className="form-input" />
+            </FormGroup>
+            <FormGroup className="form-group">
+              <FormLabel htmlFor="email" className="form-label mx-1">Email</FormLabel>
+              <FormControl required name="email" id="email" type="text" className="form-input" />
+            </FormGroup>
+            <FormGroup>
+              <FormLabel htmlFor="textarea" className="form-label mx-1">Your message</FormLabel>
+              <FormControl as="textarea" required cols="50" rows="10" id="textarea" name="textarea" className="form-textarea" />
+            </FormGroup>
+            <Button type="submit" className="form-submit-btn">Submit</Button>
+          </Form>
+        </Container>
+      </Col>
+      <Col lg={4}>
+        {/* Icons */}
+      </Col>
+    </Row>  
   )
 }
 

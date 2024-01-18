3d27b86ee6de4e36876d62e8de62936ae95138df
Máté Szakasits <120025444+mate153@users.noreply.github.com>
2024-01-18 23:28:39 +0100

    [ UPDATE ] react-i18next implemented / translation props added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,9 @@
 import { Container } from 'react-bootstrap';
+import { useTranslation } from "react-i18next";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import translationEN from "./locales/en/translation.json";
+import translationHU from "./locales/hu/translation.json";
 import AboutMeSection from './components/AboutMeSection.jsx'
 import ContactSection from './components/ContactSection.jsx'
 import NavbarSection from './components/NavbarSection.jsx'
@@ -8,38 +13,57 @@ import FooterSection from './components/FooterSection.jsx'
 import ScrollToTopButton from './components/ScrollToTopButton.jsx'
 import './App.css'
 
+const resources = {
+  en: {
+    translation: translationEN,
+  },
+  hu: {
+    translation: translationHU,
+  },
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: "en",
+  fallbackLng: "en",
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
 function App() {
+  const { t } = useTranslation();
 
   return (
     <Container className='bg-dark mt-auto' fluid style={{ padding: 0, margin: 0, width: '100%' }}>
       {/*NAVBAR SECTION*/}
-      <NavbarSection />
+      <NavbarSection translation={t} />
 
       {/*WELCOME SECTION*/}
       <section id="welcome-section">
-        <WelcomeSection />
+        <WelcomeSection translation={t} />
       </section>
 
       {/*ABOUT ME SECTION*/}
       <section id="about-me-section">
-        <AboutMeSection />
-      </section>
+        <AboutMeSection translation={t} />
+      </section> 
 
       {/*PROJECTS SECTION*/}
       <section id="projects-section">
-        <ProjectsSection />
+        <ProjectsSection translation={t} />
       </section>
 
       {/*CONTACT SECTION*/}
       <section id="contact-section">
-        <ContactSection />
+        <ContactSection translation={t} />
       </section>
 
       {/*SCROLL TO TOP BUTTON*/}
-      <ScrollToTopButton />
+      <ScrollToTopButton translation={t} />
 
       {/*FOOTER SECTION*/}
-      <FooterSection />
+      <FooterSection translation={t} />
     </Container>
   )
 }
